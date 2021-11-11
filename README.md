9b9197e749097ee968fd13a9d3d90cce56b41b99
Milenasilvadefreitas170 <milenasilvadefreitas170@gmail.com>
2021-11-10 21:37:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-3ytpew
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-3ytpew)
---
+++ index.ts
@@ -0,0 +1,6 @@
+/* Questão 2 Milena Freitas da Silva */
+
+const modulo1: number = parseInt(prompt('M1: '));
+const modulo2: number = parseInt(prompt('M2: '));
+let media: number = (modulo1 * 4 + modulo2 * 6) / 10;
+document.write('<p>Média: ' + media);
